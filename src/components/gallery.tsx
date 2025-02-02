f6c53c8e52edb074a106289875d96f2b9e602180
Jørgen Kleppan <jokle5828@oslomet.no>
2025-02-03 00:04:30 +0100

    analytics

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 interface ImageType {
   id: number;
@@ -13,13 +14,18 @@ interface GalleryProps {
 
 export const Gallery: React.FC<GalleryProps> = ({ images }) => {
   const [selectedImage, setSelectedImage] = useState<ImageType | null>(null);
+  const [overlayVisible, setOverlayVisible] = useState(false);
 
   const handleImageClick = (image: ImageType) => {
     setSelectedImage(image);
+    setOverlayVisible(true);
   };
 
   const handleCloseOverlay = () => {
-    setSelectedImage(null);
+    setOverlayVisible(false);
+    setTimeout(() => {
+      setSelectedImage(null);
+    }, 300); // Samme varighet som CSS transition
   };
 
   return (
@@ -36,8 +42,8 @@ export const Gallery: React.FC<GalleryProps> = ({ images }) => {
               alt={image.alt}
               className="w-56 object-contain rounded-lg drop-shadow-lg hover:drop-shadow-2xl shadow-black z-40"
             />
-            <div className="absolute bg-black inset-0 rounded-lg opacity-0 group-hover:opacity-70 transition-all duration-500 flex justify-center items-center">
-              <h2 className="text-white text-2xl font-bold">{image.title}</h2>
+            <div className="absolute bg-black inset-0 rounded-lg opacity-0 group-hover:opacity-70 transition-all duration-500 flex justify-center items-center cursor-pointer">
+              <FaArrowRight size={50} className="text-white" />
             </div>
           </div>
         ))}
@@ -46,19 +52,25 @@ export const Gallery: React.FC<GalleryProps> = ({ images }) => {
       {selectedImage && (
         <div
           onClick={handleCloseOverlay}
-          className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50"
+          className={`fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50 transition-opacity duration-500 ${
+            overlayVisible ? "opacity-100" : "opacity-0"
+          }`}
         >
           <div className="relative">
             <button
               className="absolute top-2 right-2 text-white text-3xl"
-              onClick={handleCloseOverlay}
+              onClick={(e) => {
+                e.stopPropagation();
+                handleCloseOverlay();
+              }}
             >
               &times;
             </button>
             <img
               src={selectedImage.url}
               alt={selectedImage.alt}
-              className="w-1/2 mx-auto rounded-3xl  max-h-full object-contain"
+              className="w-1/2 rounded-3xl  h-fit mx-auto object-contain transition-transform duration-500"
+              style={{ transform: overlayVisible ? "scale(1)" : "scale(0.9)" }}
             />
           </div>
         </div>
