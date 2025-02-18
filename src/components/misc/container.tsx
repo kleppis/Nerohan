5364d3762df5e30a18dfd038f87bf6c4a730b2e2
Jørgen Kleppan <jokle5828@oslomet.no>
2025-02-18 21:28:33 +0100

    Refactor Prosjekter page: simplify layout and integrate Container component for project display

--- src/components/misc/container.tsx
+++ src/components/misc/container.tsx
@@ -4,6 +4,10 @@ interface ContainerProps {
 
 export const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8">{children}</div>
+    <div className=" border-text border-2 rounded-lg mx-auto p-4 overflow-hidden group/container">
+      <div className="group-hover/container:scale-110 transition-all duration-500">
+        {children}
+      </div>
+    </div>
   );
 };
--- src/pages/prosjekter.tsx
+++ src/pages/prosjekter.tsx
@@ -1,71 +1,24 @@
-import { useState, useRef, useEffect } from "react";
 import { Nav } from "../components/nav";
-import ProsjektLayout from "../components/prosjekt-komponenter/prosjektLayout";
 import Footer from "../components/footer-comp/footer";
+import PageLayout from "../components/pageLayout";
+import { ulikeProsjekter } from "../constants/prosjekter";
+import { Container } from "../components/misc/container";
 
 const Prosjekter = () => {
-  const [activeButton, setActiveButton] = useState<number | null>(null);
-  const [underlineStyle, setUnderlineStyle] = useState({});
-  const buttonsRef = useRef<(HTMLButtonElement | null)[]>([]);
-
-  const handleButtonClick = (index: number) => {
-    setActiveButton(index);
-  };
-
-  useEffect(() => {
-    if (activeButton !== null && buttonsRef.current[activeButton]) {
-      const button = buttonsRef.current[activeButton];
-      setUnderlineStyle({
-        width: button.offsetWidth,
-        left: button.offsetLeft,
-      });
-    }
-  }, [activeButton]);
-
-  const renderContent = () => {
-    switch (activeButton) {
-      case 0:
-        return <ProsjektLayout />;
-      case 1:
-        return <div>Innhold for Silurveien 324</div>;
-      case 2:
-        return <div>Innhold for Silurveien 325</div>;
-      default:
-        return <div>Velg et prosjekt for å se detaljer</div>;
-    }
-  };
-
   return (
     <>
       <Nav title="Prosjekter" />
-      <div>
-        <img
-          src="/assets/makita.jpg"
-          alt=""
-          className="w-full mx-auto h-[50vh] object-cover -translate-y-24"
-        />
-      </div>
-      <div className="max-w-7xl mx-auto py-3">
-        <div className="relative flex space-x-4">
-          {["Silurveien 323", "Silurveien 324", "Silurveien 325"].map(
-            (button, index) => (
-              <button
-                key={index}
-                onClick={() => handleButtonClick(index)}
-                ref={(el) => (buttonsRef.current[index] = el)}
-                className="relative w-1/3 text-2xl text-text uppercase py-3 "
-              >
-                {button}
-              </button>
-            )
-          )}
-          <div
-            className="absolute bottom-0 h-1 bg-text transition-all duration-300"
-            style={underlineStyle}
-          ></div>
+      <PageLayout imgUrl="/assets/makita.jpg" alt="Makita">
+        <h1>Prosjekter</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
+          {ulikeProsjekter.map((prosjekt) => (
+            <Container>
+              <h2>{prosjekt.tittel}</h2>
+              <p>{prosjekt.beskrivelse}</p>
+            </Container>
+          ))}
         </div>
-        <div className="mt-10">{renderContent()}</div>
-      </div>
+      </PageLayout>
       <Footer />
     </>
   );
