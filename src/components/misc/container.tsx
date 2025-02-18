be30d2f963a068db2a97b7f506e25d32b90f6d39
Jørgen Kleppan <jokle5828@oslomet.no>
2025-02-18 21:01:24 +0100

    Add Container component and prosjekt constants for project details

---
+++ src/components/misc/container.tsx
@@ -0,0 +1,9 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+export const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8">{children}</div>
+  );
+};
